f0d111ac3850a365ee7d8dac7ec7e604ba4540bb
TechProCan <102727118+TechProCan@users.noreply.github.com>
2023-01-04 14:32:45 +0300

    deneme

---
+++ Selenium_4_7_0/src/test/java/firstTest/Chrome_Driver.java
@@ -0,0 +1,14 @@
+package firstTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Chrome_Driver {
+    public static void main(String[] args) {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://techproeducation.com");
+
+    }
+
+
+}
---
+++ Selenium_4_7_0/src/test/java/firstTest/Selenium_Manager.java
@@ -0,0 +1,18 @@
+package firstTest;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.manager.SeleniumManager;
+
+public class Selenium_Manager {
+    public static void main(String[] args) {
+        SeleniumManager.getInstance().getDriverPath("chromedriver");
+        ChromeDriver driver=new ChromeDriver();
+        driver.get("https://www.techproeducation.com");
+
+        SeleniumManager.getInstance().getDriverPath("IEDriverServer");
+        InternetExplorerDriver driver1=new InternetExplorerDriver();
+        driver.get("https://www.techproeducation.com");
+
+    }
+}
